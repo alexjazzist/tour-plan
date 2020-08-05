a2c75ac44030ee8093a413875cc685601f96a41f
alexjazzist <alexjazzist@gmail.com>
2020-08-05 22:38:34 +0300

    added form validation

--- js/main.js
+++ js/main.js
@@ -45,10 +45,9 @@ $(document).ready(function () {
   closeModalButton.on("click", closeModal);
 
   function openModal() {
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    modalOverlay.addClass("modal__overlay--visible");
-    modalDialog.addClass("modal__dialog--visible");
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
   }
   function closeModal(event) {
     event.preventDefault();
@@ -58,4 +57,26 @@ $(document).ready(function () {
     modalDialog.removeClass("modal__dialog--visible");
   }
 
+  //Обработка форм
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Укажите имя",
+          minlength: "Имя не короче 2-х букв",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Телефон обязателен",
+        },
+        rating: {
+          required: "Укажите цифру от 1 до 5",
+        },
+      },
+    });
+  });
 });
